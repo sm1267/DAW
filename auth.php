ca19b31ca5889a3edbc57ba7df5e56b1f56decfa
sm1267 <94132306+sm1267@users.noreply.github.com>
2024-12-19 22:49:00 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GYM DAW</title>
     <link rel="stylesheet" href="style.css">
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
     <div class="hero">
@@ -17,17 +18,30 @@
             <form action="includes/login.inc.php"method="post" id="login" class="input-group">
                 <input type="text" name="uid" class="input-field" placeholder="Username" required>
                 <input type="password" name="pwd" class="input-field" placeholder="Password" required>
+                <div class="g-recaptcha" data-sitekey="6LeCqKAqAAAAAAMmJ86A9HwukcmBCQhsPr3epd-H"></div>
                 <button type="submit" name="submit" class="submit-btn">Log in</button>
             </form>
             <form action="includes/signup.inc.php"method="post" id="register" class="input-group">
                 <input type="text" name="uid" class="input-field" placeholder="Username" required>
                 <input type="password" name="pwd" class="input-field" placeholder="Password" required>
                 <input type="password" name="pwdrepeat" class="input-field" placeholder="Repeat Password" required>
+                <div class="g-recaptcha" data-sitekey="6LeCqKAqAAAAAAMmJ86A9HwukcmBCQhsPr3epd-H"></div>
                 <button type="submit" name="submit" class="submit-btn">Register</button>
             </form>
         </div>
     </div>
     <script>
+    document.querySelectorAll('form').forEach(form => {
+    form.addEventListener('submit', (e) => {
+        const captchaResponse = grecaptcha.getResponse();
+
+        if (!captchaResponse) {
+            e.preventDefault();
+            alert("Please complete the reCAPTCHA before submitting.");
+        }
+        });
+    });
+
     var x = document.getElementById("login");
     var y = document.getElementById("register");
     var z = document.getElementById("btn");
